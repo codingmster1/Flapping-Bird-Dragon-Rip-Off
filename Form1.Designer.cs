9f243f6862219840be40f15f75c21349cbf37aed
Jeff Doyle <doylejeff98@gmail.com>
2020-11-30 17:35:55 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Game_test_FB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.pipe2 = new System.Windows.Forms.PictureBox();
+            this.pipe1 = new System.Windows.Forms.PictureBox();
+            this.Bird = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bird)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(100, 384);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(94, 25);
+            this.scoreText.TabIndex = 4;
+            this.scoreText.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // ground
+            // 
+            this.ground.Image = global::Game_test_FB.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(0, 380);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(579, 39);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 3;
+            this.ground.TabStop = false;
+            // 
+            // pipe2
+            // 
+            this.pipe2.Image = global::Game_test_FB.Properties.Resources.pipe;
+            this.pipe2.Location = new System.Drawing.Point(200, 258);
+            this.pipe2.Name = "pipe2";
+            this.pipe2.Size = new System.Drawing.Size(97, 242);
+            this.pipe2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipe2.TabIndex = 2;
+            this.pipe2.TabStop = false;
+            // 
+            // pipe1
+            // 
+            this.pipe1.Image = global::Game_test_FB.Properties.Resources.pipedown;
+            this.pipe1.Location = new System.Drawing.Point(272, -109);
+            this.pipe1.Name = "pipe1";
+            this.pipe1.Size = new System.Drawing.Size(97, 208);
+            this.pipe1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipe1.TabIndex = 0;
+            this.pipe1.TabStop = false;
+            this.pipe1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Bird
+            // 
+            this.Bird.Image = global::Game_test_FB.Properties.Resources._1;
+            this.Bird.Location = new System.Drawing.Point(12, 120);
+            this.Bird.Name = "Bird";
+            this.Bird.Size = new System.Drawing.Size(85, 73);
+            this.Bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Bird.TabIndex = 1;
+            this.Bird.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(378, 418);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipe2);
+            this.Controls.Add(this.pipe1);
+            this.Controls.Add(this.Bird);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird Clone";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipe1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bird)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pipe1;
+        private System.Windows.Forms.PictureBox Bird;
+        private System.Windows.Forms.PictureBox pipe2;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_test_FB
+{
+    public partial class Form1 : Form
+    {
+        int pipeSpeed = 6;
+        int gravity = 8;
+        int score = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            Bird.Top += gravity;
+            pipe1.Left -= pipeSpeed;
+            pipe2.Left -= pipeSpeed;
+            scoreText.Text = "Score: " + score;
+
+            if (pipe1.Left < -150)
+            {
+
+                pipe1.Left = 800;
+                score++;
+            }
+            if (pipe2.Left < -190)
+            {
+                pipe2.Left = 950;
+                score++;
+
+            }
+            if (score > 10)
+            {
+                pipeSpeed = 12;
+            }
+            if (score > 25)
+            {
+                pipeSpeed = 16;
+            }
+            if (score > 35)
+            {
+                pipeSpeed = 20;
+            }
+            if (score > 60)
+            {
+                pipeSpeed = 25;
+            }
+            if (score > 100)
+            {
+                pipeSpeed = 28;
+            }
+            if (score > 125)
+            {
+                pipeSpeed = 15;
+            }
+            if (score > 140)
+            {
+                pipeSpeed = 10;
+            }
+            if (score > 160)
+            {
+                pipeSpeed = 30;
+            }
+            if (score > 200)
+            {
+                pipeSpeed = 35;
+            }
+            if (Bird.Top < -25)
+            {
+                endGame(); 
+            }
+
+            if (Bird.Bounds.IntersectsWith(pipe1.Bounds) ||
+                Bird.Bounds.IntersectsWith(pipe2.Bounds) ||
+                Bird.Bounds.IntersectsWith(ground.Bounds) || Bird.Top < -25  
+                )
+            {
+                endGame();
+            }
+        }
+
+    
+  
+            
+           
+            
+
+      
+
+
+        private void gameKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -8;
+            }
+        }
+
+        private void gameKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 8;
+            }
+
+            {
+
+            }
+        }
+        private void endGame()
+        {
+            gameTimer.Stop();
+            scoreText.Text += "Game Over";
+        }
+    }
+
+
+}
